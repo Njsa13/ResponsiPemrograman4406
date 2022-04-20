5f08d8e8cc35407d4f17151050b2112ceeb80a95
Najib Sauqi R <97015365+Njsa13@users.noreply.github.com>
2022-04-20 09:41:38 +0700

    menambah file .cs

---
+++ Karyawan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4406
+{
+    public class Karyawan
+    {
+        //properties
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        //Method
+        public void DaftarGaji(int no)
+        {
+            //apabila gaji kurang dari nol maka secara otomatik akan di set menjadi nol
+            if(GajiBulanan <= 0)
+            {
+                GajiBulanan = 0;
+            }
+            Console.WriteLine("{0}. {1} {2}                 {3}", no, Nik, Nama, GajiBulanan);
+        }
+
+        public void DaftarGajiNew(int no)
+        {
+            //apabila gaji kurang dari nol maka secara otomatik akan di set menjadi nol
+            if (GajiBulanan <= 0)
+            {
+                GajiBulanan = 0;
+            }
+            int GajiBulananNew = GajiBulanan + (GajiBulanan*10)/100;
+            Console.WriteLine("{0}. {1} {2}                 {3}", no, Nik, Nama, GajiBulananNew);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4406
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constructor
+            Karyawan karyawan1 = new Karyawan();
+            Karyawan karyawan2 = new Karyawan();
+
+            //properties
+            karyawan1.Nik = "123456789";
+            karyawan1.Nama = "Najib";
+            karyawan1.GajiBulanan = 2000000;
+
+            karyawan2.Nik = "987654321";
+            karyawan2.Nama = "Sauqi";
+            karyawan2.GajiBulanan = 3000000;
+
+            //method
+            Console.WriteLine("No Nik/Nama                  Gaji Bulanan");
+            Console.WriteLine("-----------------------------------------");
+            karyawan1.DaftarGaji(1);
+            karyawan2.DaftarGaji(2);
+            Console.WriteLine();
+
+            //kenaikan gaji
+            Console.WriteLine("Asiiik kenaikan gaji 10% !!!!!");
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama                  Gaji Bulanan");
+            Console.WriteLine("-----------------------------------------");
+            karyawan1.DaftarGajiNew(1);
+            karyawan2.DaftarGajiNew(2);
+
+            Console.ReadKey();
+        }
+    }
+}
